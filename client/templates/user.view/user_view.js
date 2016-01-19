d8e98b64defc021a21a4d4df39fcb643dc03caea
jona <jona@jona.io>
2016-01-19 17:16:09 +0100

    move the long comment down to the function

--- client/templates/user.view/user_view.js
+++ client/templates/user.view/user_view.js
@@ -10,18 +10,18 @@ Template.user_view.helpers({
     return thisUser()
   },
   gravatar () {
-    // let's take a short break to discuss: why not 'user = Users.find()[0]'?
-    // there's only one user subscribed to, right?
-    // no, actually. see what happens when you execute Meteor.user()? that's
-    // right, you get a user entry. now this isn't stored in a separate
-    // collection, oh no. it's always stored in Meteor.users aka. Users.
-    // because of that, you might get your own user object back when you get
-    // the user with that query.
     let user = thisUser()
     return Gravatar.imageUrl(user.emails[0].address, { size: 100 })
   }
 })
 
 const thisUser = () => {
+  // let's take a short break to discuss: why not 'user = Users.find()[0]'?
+  // there's only one user subscribed to, right?
+  // no, actually. see what happens when you execute Meteor.user()? that's
+  // right, you get a user entry. now this isn't stored in a separate
+  // collection, oh no. it's always stored in Meteor.users aka. Users.
+  // because of that, you might get your own user object back when you get
+  // the user with that query.
   return Users.findOne({username: FlowRouter.getParam('username')})
 }
