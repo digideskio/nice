94bca2b1b6916b62818748f7b020a543266a3fa6
jona <jona@jona.io>
2016-01-20 22:24:45 +0100

    add additional security rule

--- server/security.js
+++ server/security.js
@@ -6,9 +6,21 @@ Security.defineMethod('ifIsSameUser', {
   }
 })
 
+Security.defineMethod('ifChangesSelf', {
+  fetch: [],
+  transform: null,
+  deny (type, arg, userId, doc) {
+    const origUser = Users.findOne({_id: doc._id})
+    if (_.difference(origUser.followers, doc.followers).length === 1 &&
+        _.difference(origUser.followers, doc.followers)[0] === userId)
+      return false
+    return true
+  }
+})
+
 // users can create new updates
 Updates.permit(['insert']).ifLoggedIn().apply()
 
 // logged in users can add themselves to follower lists of other users
 Users.permit(['update']).ifLoggedIn().ifIsSameUser().apply()
-Users.permit(['update']).ifLoggedIn().onlyProps(['followers']).apply()
+Users.permit(['update']).ifLoggedIn().ifChangesSelf().onlyProps(['followers']).apply()
