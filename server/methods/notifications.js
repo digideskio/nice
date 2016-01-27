ac98a471ac0e557b331be121ae2a392b4f942768
jona <jona@jona.io>
2016-01-27 19:12:29 +0100

    add notification insertion method

---
+++ server/methods/notifications.js
@@ -0,0 +1,16 @@
+Meteor.methods({
+	'notifications.new' (targetId, type, content) {
+		let user = Users.findOne({_id: this.userId})
+		let doc = {
+			to: targetId,
+			from: {
+				_id: user._id,
+				username: user.username,
+				email: user.emails[0].address
+			},
+			type,
+			content
+		}
+		Notifications.insert(doc)
+	}
+})
